8e64e8ba865a7fa19e60f53464805d3f1ca5c09d
IzadoraSC <izadoraflorestal@gmail.com>
2022-05-26 12:03:16 -0300

    implementando geração de gráficos

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -7,9 +7,18 @@
 
 
 
-library()
 rm(list = ls()) # to remove objects from environment (a character 
 #ector naming objects to be removed)
+
+##Criando um projeto: dentro da pasta GitHub, crie um projeto. Esse sera o nome da pasta criada e do repositorio no GitHub.
+#usethis::create_project("~/GitHub/research_data_analysis_pncm")
+#usethis::use_git() #para ativar o painel "Git"
+#usethis::use_github() #para criar o repositorio na sua conta do GitHub
+#usethis::use_readme_md() #funcao para criar um arquivo README.md
+##para criar um projeto a partir um repositorio existente:
+#usethis::create_from_github("R-Ladies-Sao-Paulo/RLadies-Brasil",
+                            #destdir = "C:/Users/beatr/Documents/GitHub", 
+                            #fork = TRUE)
 
 #update.packages(checkBuilt=TRUE, ask=FALSE)
 ###Packages
@@ -86,6 +95,7 @@ df.summarise <- ddply(df_geral, .(vegetation, last_fire, Area), summarise,
 qnt_plots <- ddply(df_geral, .(vegetation, last_fire), summarise,
                       Qnt_plot=length(unique(Parcela_2)))
 
+#Gerando tabela
 sum(qnt_plots$Qnt_plot)
 table_qnt_plots <- grid.table(qnt_plots)
 table_qnt_plots
@@ -115,35 +125,42 @@ bartlett.test(T_Max ~ vegetation, data=df.summarise)
 t.h5 <- plot(T_Max ~ vegetation, data=df.summarise)
 
 bartlett.test(T_Min ~ vegetation, data=df.summarise)
+t.h6 <- plot(T_Min ~ vegetation, data=df.summarise)
 
 bartlett.test(T_Mean ~ vegetation, data=df.summarise)
+t.h7 <- plot(T_Mean ~ vegetation, data=df.summarise)
 
 bartlett.test(Diameter ~ vegetation, data=df.summarise)
+t.h8 <- plot(Diameter ~ vegetation, data=df.summarise)
 
 bartlett.test(Height ~ vegetation, data=df.summarise)
+t.h9 <- plot(Height ~ vegetation, data=df.summarise)
 
 bartlett.test(SPAD ~ vegetation, data=df.summarise)
-plot(SPAD ~ vegetation, data=df.summarise)
+t.h10 <- plot(SPAD ~ vegetation, data=df.summarise)
 
 #Box plots with p-values
-bxp <- ggboxplot(df.summarise, x = "vegetation", y = "SPAD")
-bxp
-t_spad <- t_spad %>% add_xy_position(x = "supp")
-bxp + 
-  stat_pvalue_manual(t_spad, label = "p") +
-  scale_y_continuous(expand = expansion(mult = c(0.05, 0.1)))
+# bxp <- ggboxplot(df.summarise, x = "vegetation", y = "SPAD")
+# bxp
+# t_spad <- t_spad %>% add_xy_position(x = "supp")
+# bxp + 
+#   stat_pvalue_manual(t_spad, label = "p") +
+#   scale_y_continuous(expand = expansion(mult = c(0.05, 0.1)))
 
 
 #Verificao da normalidade dos dados
 glimpse(df.summarise)
 
-shapiro.test(PNCM_annual.park$Scars)
-shapiro.test(PNCM_annual.park$Total_BA)
-shapiro.test(PNCM_annual.park$percentBA)
-shapiro.test(PNCM_annual.park$MeanArea)
-shapiro.test(PNCM_annual.park$MaxArea)
-shapiro.test(PNCM_annual.park$SdBA)
-shapiro.test(PNCM_annual.park$IgnBA)
+shapiro.test(df.summarise$FvFm)
+shapiro.test(df.summarise$RCABS)
+shapiro.test(df.summarise$FvFo)
+shapiro.test(df.summarise$PI)
+shapiro.test(df.summarise$T_Max)
+shapiro.test(df.summarise$T_Min)
+shapiro.test(df.summarise$T_Mean)
+shapiro.test(df.summarise$Diameter)
+shapiro.test(df.summarise$Height)
+shapiro.test(df.summarise$SPAD)
 
 
 
@@ -165,7 +182,7 @@ new_data <- new_data[order(new_data$last_fire)]
 
 labels2 <- data.frame(new_data$groups)
 
-g3 <- boxplot(Diameter ~ Vegetation, data = df.summarise, 
+g3 <- boxplot(Diameter ~ vegetation, data = df.summarise, 
               #ylim=c(min(df.summarise$SPAD), 1.1*max(df.summarise$SPAD)),
               #col=c("black", "red", "blue", "green"),
               ylab="Diameter (cm)", xlab="Last Fire", main= "Boxplots", las = 1)
@@ -183,21 +200,21 @@ library(multcompView)
 # setdw("....")
 ########
 b0 <- ggplot(df.summarise) +
-  aes(x = last_fire, y = Diameter, fill = last_fire) +
+  aes(x = last_fire, y = FvFm, fill = last_fire) +
   geom_boxplot() +
   #geom_boxplot(show.legend = FALSE) +
   #scale_fill_manual(values=c("#A6611A","#DFC27D")) +
-  ylab('Diameter (cm)') +  xlab('') + 
+  ylab('Rendimento quântico máximo do FSII (Fv/Fm)') +  xlab('Last Fire') + 
   guides(x = guide_axis(angle = -45)) +
   #scale_y_continuous(breaks = seq(0,100,10)) + 
   theme_bw() + 
-  theme(axis.title.x = element_text(colour="black", size=20,
+  theme(axis.title.x = element_text(colour="black", size=12,
                                     face="bold")) +
-  theme(axis.title.y = element_text(colour="black", size=20,
+  theme(axis.title.y = element_text(colour="black", size=12,
                                     face="bold", vjust = 2.0)) +
-  theme(axis.text = element_text(colour = "black", size = 16)) +
+  theme(axis.text = element_text(colour = "black", size = 12)) +
   theme(panel.grid = element_line(color = "white")) +
-  facet_wrap(vars(Vegetation))
+  facet_wrap(vars(vegetation))
 
 
 b0
@@ -218,7 +235,7 @@ b1 <- ggplot(df.summarise) +
                                     face="bold", vjust = 2.0)) +
   theme(axis.text = element_text(colour = "black", size = 16)) +
   theme(panel.grid = element_line(color = "white")) +
-  facet_wrap(vars(Vegetation))
+  facet_wrap(vars(vegetation))
 
 
 b1
